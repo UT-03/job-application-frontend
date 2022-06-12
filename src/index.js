c8c0d3ad2929af01e73c937dfb6685b42ec7cd70
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-06-12 12:38:44 +0530

    feat: add sass customizable react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style.scss';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root'))
